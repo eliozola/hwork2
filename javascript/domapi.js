25925eddee8367c01846ca1a972e2b2d69f71e9b
eliozola <ozola.eli@gmail.com>
2020-06-02 11:09:37 +0300

    commit

--- javascript/domapi.js
+++ javascript/domapi.js
@@ -1,1 +1,13 @@
-console.log("Running domapi examples");
+function main() {
+    console.log("Running domapi examples");    
+    console.log("Running main");  
+    const app = document.getElementById('myApp');
+    app.style.height = "200px"; 
+    app.style.backgroundColor = "rgb(219, 127, 142)"; 
+    app.innerText = "My big bad application";
+    app.style.border = "bold";
+    app.style.width = "100%";
+    const boxes = document.getElementsByClassName("boxy");
+}
+
+main();
