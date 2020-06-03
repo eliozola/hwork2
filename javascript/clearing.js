8fe2ecc072ffd1a467bc83461943c01e5ca26fe7
eliozola <ozola.eli@gmail.com>
2020-06-03 12:24:03 +0300

    added css

---
+++ javascript/clearing.js
@@ -0,0 +1,15 @@
+console.log("Started clearing js");
+
+function addElements() {
+
+    console.log("Adding elements");
+}
+
+function deleteElements() {
+    console.log("Clearing elements")
+}
+
+function addEventHandlers() {
+    console.log("adding Handlers");
+    const addBtn = document.getElementById("btn-id-add");
+}
--- javascript/events.js
+++ javascript/events.js
@@ -1,3 +1,15 @@
 function onClick() {
     console.log("Something was clicked");
 }
+function addEventHandlers () {
+    console.log("Adding Event Handlers");
+    const myEl = document.getElementById("box-id01");
+    myEl.onclick - onMyClick;
+    document.getElementById("box-id02").onclick = onBoxClick;
+    document.getElementById("box-id03").onclick = () =>
+    console.log("Clicked Box 3");
+}
+
+function addSecondaryHandlers() {
+    const myElements = document.getElementsByClassName("secondary");
+}
